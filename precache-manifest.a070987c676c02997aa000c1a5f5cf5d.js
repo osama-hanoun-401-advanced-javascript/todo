b3a74e8b429ee087a896fde108314710b40dd205
OsamaHanoun <OsamaHanoun@users.noreply.github.com>
2020-10-19 05:05:13 +0000

    deploy: d5a8f278fb8b90ffcedbf8985614ec75a4df13c4

---
+++ precache-manifest.a070987c676c02997aa000c1a5f5cf5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761fd4d96c98f1876500d5b5330c73cf",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "a983f444b772312fd60a",
+    "url": "/todo/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "dd158858fe1c49b04556",
+    "url": "/todo/static/css/main.fe85d6eb.chunk.css"
+  },
+  {
+    "revision": "a983f444b772312fd60a",
+    "url": "/todo/static/js/2.8889349f.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/todo/static/js/2.8889349f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd158858fe1c49b04556",
+    "url": "/todo/static/js/main.3e65e8d0.chunk.js"
+  },
+  {
+    "revision": "4b7c96a078d3292937b1",
+    "url": "/todo/static/js/runtime-main.d4ce9cd5.js"
+  }
+]);
